26bb8eba14ae8508dc78fa2338dd9528cfd52775
Tshepisoh <thlahla4@gmail.com>
2025-09-22 12:24:49 +0200

    Update Products.jsx

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import LeftBar from "../components/SalesSummary";
 import ProductsMain from "../components/ProductsMain";
-import { Grid, Typography } from "@mui/material";
-
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
-import Paper from '@mui/material/Paper';
+import { Grid } from "@mui/material";
 
 const Products = () => {
   return (
     <div>
-  
-        <NavBar>
-        </NavBar>
-      <Grid container sx={{ml:4}}>
+      <NavBar></NavBar>
+      <Grid container sx={{ ml: 4 }}>
         <Grid item sm={12} sx={{ background: "#ececec", height: "100vh" }}>
           <ProductsMain />
         </Grid>
